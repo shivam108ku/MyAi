5be374b9db57232b7058b17d4f2f7f2e6f037363
Shivam <shivam108ku@gmail.com>
2026-04-04 12:11:45 +0530

    feat: add support for text splitting and update pdf-parse dependency

--- prepare.js
+++ prepare.js
@@ -1,7 +1,18 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf"
+import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters'
 
 export async function indexTheDocument(filePath){
     const loader = new PDFLoader(filePath, {splitPages: false});
     const doc = await loader.load();
-    console.log(doc[0].pageContent);
+    const document = doc[0].pageContent;
+
+    const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 100,
+    chunkOverlap: 50,
+  });
+
+  const texts = await textSplitter.splitText(document);
+  console.log(texts);
+
 }
+
