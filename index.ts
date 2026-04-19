77ecfb276ebf20abd5e201f9e1b1def1706266d9
Shivam <shivam108ku@gmail.com>
2026-04-19 18:49:13 +0530

    feat: implement assistant and tool nodes with state graph for enhanced event handling

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import { ChatDeepSeek } from "@langchain/deepseek";
 import { createEventTool, getEventsTool } from "./tools";
+import { END, MessagesAnnotation, StateGraph } from "@langchain/langgraph";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import type { AIMessage } from "@langchain/core/messages";
 
 const tools: any = [createEventTool, getEventsTool];
 
@@ -7,3 +10,39 @@ const model = new ChatDeepSeek({
   model: "deepseek-chat",
   temperature: 0,
 }).bindTools(tools);
+
+// Assitant Node
+
+async function callModel(state: typeof MessagesAnnotation.State ) {
+  console.log("Calling the llm");
+  const response = await model.invoke(state.messages);
+  console.log("Response", response);
+  return { messages: response };
+}
+
+
+// Tool Node
+const toolNode = new ToolNode(tools);
+
+// Build The Graph
+
+function shouldContinue(state: typeof MessagesAnnotation.State ) {
+  const lastMessage = state.messages[state.messages.length - 1] as AIMessage;
+  console.log("lastMessage", lastMessage);
+
+  if (lastMessage.tool_calls?.length) {
+    return "tools";
+  }
+
+  return '__end__';
+}
+
+const graph = new StateGraph(MessagesAnnotation)
+  .addNode("assistant", callModel)
+  .addNode("tools", toolNode)
+  .addEdge("__start__", "assistant")
+  .addConditionalEdges("assistant", shouldContinue, {
+    "__end__": END,
+    tools: "tools",
+  })
+ 
