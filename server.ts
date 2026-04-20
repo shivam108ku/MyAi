b16f722408ae65370dae3e9b0a49a0b992c33539
Shivam <shivam108ku@gmail.com>
2026-04-20 17:09:03 +0530

    refactor: improve formatting and structure of auth routes in server.ts

--- server.ts
+++ server.ts
@@ -1,36 +1,35 @@
 import express from "express";
-import {google} from "googleapis" 
+import { google } from "googleapis";
 
 const app = express();
 
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URL
+  process.env.GOOGLE_REDIRECT_URL,
 );
 
-const url = oauth2Client.generateAuthUrl({
-  // 'online' (default) or 'offline' (gets refresh_token)
-  access_type: 'offline',
+app.get("/auth", (req, res) => {
+  const scopes = ["https://www.googleapis.com/auth/calendar"];
 
-  // If you only need one scope, you can pass it as a string
-  scope: scopes
+  const url = oauth2Client.generateAuthUrl({
+    // 'online' (default) or 'offline' (gets refresh_token)
+    access_type: "offline",
+    prompt: "consent",
+    // If you only need one scope, you can pass it as a string
+    scope: scopes,
+  });
+
+  console.log(url);
+
+  // generate link
+  const link = "";
+  res.redirect(link);
 });
 
-app.get('/auth', (req , res)=>{
+app.get("/callback", (req, res) => {
+  // generate link
+  const code = req.query.code;
 
-    // generate link
-    const link = '';
-    res.redirect(link)
-
-})
-
-app.get('/callback', (req , res)=>{
-
-    // generate link
-     const code = req.query.code;
-
-
-     res.send("Connected you can close the tab now");
-
-})
+  res.send("Connected you can close the tab now");
+});
