a8d30aa3a3e00b4dd52e93b68d2cb8c7fd22f231
Shivam <shivam108ku@gmail.com>
2026-04-23 23:21:06 +0530

    adding readline

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { createEventTool, getEventsTool } from "./tools";
 import { END, MessagesAnnotation, StateGraph } from "@langchain/langgraph";
 import { ToolNode } from "@langchain/langgraph/prebuilt";
 import type { AIMessage } from "@langchain/core/messages";
+import readline from "node:readline/promises";
 
 const tools: any = [createEventTool, getEventsTool];
 
@@ -42,23 +43,34 @@ const graph = new StateGraph(MessagesAnnotation)
 const app = graph.compile();
 
 async function main() {
-  const now = new Date().toISOString();
-
-  const result = await app.invoke({
-    messages: [
-      {
-        role: "system",
-        content: `Current datetime: ${now}`,
-      },
-      {
-        role: "user",
-        // content: "can you create a meeting with Atik(rku7693@gmail.com) today at 2PM about game? ",
-         content: "Do i have any metting today ?",
-      },
-    ],
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
 
-  console.log("Ai", result.messages[result.messages.length - 1]?.content);
+  while (true) {
+    const userInput = await rl.question("You: ");
+    if (userInput === "/bye") {
+      break;
+    }
+    const now = new Date().toISOString();
+
+    const result = await app.invoke({
+      messages: [
+        {
+          role: "system",
+          content: `Current datetime: ${now}`,
+        },
+        {
+          role: "user",
+          // content: "can you create a meeting with Atik(rku7693@gmail.com) today at 2PM about game? ",
+          content: userInput,
+        },
+      ],
+    });
+
+    console.log("Ai: ", result.messages[result.messages.length - 1]?.content);
+  }
 }
 
 main();
